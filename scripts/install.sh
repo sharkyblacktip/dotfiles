de51a596f937ae9ae6d646560003614ea06af163
sharkyblacktip <sharkyblacktip@protonmail.com>
2024-07-16 10:05:11 +0200

    updated kitty font

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,4 @@ sudo pacman -S kitty
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo pacman -S neovim
 sudo pacman -S uwufetch
+sudo pacman -S ttf-sourcecodepro-nerd
