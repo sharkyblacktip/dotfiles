1e827f0994c74c0c12b5674cc3eabc4b344fba73
sharkyblacktip <sharkyblacktip@protonmail.com>
2024-07-17 16:56:46 +0200

    Added rust lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -3,7 +3,8 @@ require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"lua_ls",
-		"html"
+		"html",
+		"rust_analyzer"
 	}
 })
 require("lspconfig").lua_ls.setup({})
@@ -12,3 +13,4 @@ require("lspconfig").lua_ls.setup({})
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['lua_ls'].setup {capabilities = capabilities}
 require('lspconfig')['html'].setup {capabilities = capabilities}
+require('lspconfig')['rust_analyzer'].setup {capabilities = capabilities}
