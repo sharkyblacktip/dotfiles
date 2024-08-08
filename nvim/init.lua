cbf525c8252dfdc1dddc5bd31b7584ef517d9b00
sharkyblacktip <sharkyblacktip@protonmail.com>
2024-08-08 12:04:43 +0200

    Tweaked file finder keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,5 +21,5 @@ require("config.plugin-startup")
 
 -- keybinds
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader><leader>', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>b', telescope_builtin.buffers, {})
